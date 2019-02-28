e6fd7f53dd6389b251237f20a36243f8a9b56370
usuario <usuario@LENOVO3>
2019-02-28 13:20:24 +0100

    Ejercicio 7 Tema 7. Modifico el ModeloReservasAulas y el IModeloReservasAulas.

---
+++ src/main/java/org/iesalandalus/programacion/reservasaula/modelo/IModeloReservasAulas.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.reservasaula.modelo;
+
+/**
+ *
+ * @author usuario
+ */
+import java.util.List;
+import javax.naming.OperationNotSupportedException;
+import org.iesalandalus.programacion.reservaaulas.modelo.dominio.Aula;
+import org.iesalandalus.programacion.reservaaulas.modelo.dominio.Profesor;
+import org.iesalandalus.programacion.reservaaulas.modelo.dominio.Reserva;
+import org.iesalandalus.programacion.reservasaulas.modelo.dominio.permanencia.Permanencia;
+
+
+
+/**
+ *
+ * @author usuario
+ */
+public interface IModeloReservasAulas {
+
+    void anularReserva(Reserva reserva) throws OperationNotSupportedException;
+
+    void borrarAula(Aula aula) throws OperationNotSupportedException, IllegalArgumentException;
+
+    void borrarProfesor(Profesor profesor) throws OperationNotSupportedException;
+
+    Aula buscarAula(Aula aula);
+
+    Profesor buscarProfesor(Profesor profesor);
+
+    Reserva buscarReserva(Reserva reserva);
+
+    boolean consultarDisponibilidad(Aula aula, Permanencia permanencia);
+
+    //Aulas
+    //Revisar gett
+    List<Aula> getAulas();
+
+    int getNumAulas();
+
+    int getNumProfesores();
+
+    int getNumReservas();
+
+    //Profesores
+    List<Profesor> getProfesores();
+
+    // Reservas
+    List<Reserva> getReservas();
+
+    List<Reserva> getReservasAula(Aula aula);
+
+    List<Reserva> getReservasPermanencia(Permanencia permanencia);
+
+    List<Reserva> getReservasProfesores(Profesor profesor);
+
+    void insertarAula(Aula aula) throws OperationNotSupportedException, IllegalArgumentException;
+
+    void insertarProfesor(Profesor profesor) throws OperationNotSupportedException;
+
+    void realizarReserva(Reserva reserva) throws OperationNotSupportedException;
+
+    List<String> representarAulas();
+
+    List<String> representarProfesores();
+
+    List<String> representarReservas();
+    
+    void leerAulas();
+	
+    void escribirAulas();
+    
+    void leerProfesores();
+	
+    void escribirProfesores();
+    
+    void leerReservas();
+	
+    void escribirReservas();
+    
+}
+
